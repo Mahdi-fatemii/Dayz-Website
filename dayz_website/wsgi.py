5a548917652a7176dec70a414e17fe76ccfc3fff
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2024-01-22 23:08:23 +0330

    first commit

---
+++ dayz_website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dayz_website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dayz_website.settings')
+
+application = get_wsgi_application()
